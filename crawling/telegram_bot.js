b53400cc133a1e0042d479fe24f1363f8e749d86
Andre Kenji Yai <yai.andre@gmail.com>
2017-06-26 15:28:33 -0300

    Organizing Files

---
+++ crawling/telegram_bot.js
@@ -0,0 +1,58 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Reddit = require('./crawling');
+const token = '438921418:AAGFFZPDv-HkJgcO-tnLiXR7XwItvgE48RQ';
+const bot = new TelegramBot(token, {polling: true});
+
+
+sendMessage = function(chatId,message,...extraField){
+  bot.sendMessage(chatId,message,extraField[0]);
+}
+
+sendFailedMessage = function(chatId,error){
+  let errorMessage = "There was an error."
+  sendMessage(chatId,errorMessage)
+}
+
+sendSuccesfulMessage = function(chatId,topThreads){
+    topThreads.forEach(function(subReddit){
+      subRedditName = Object.keys(subReddit)[0];
+      if(subReddit[subRedditName] == 0){
+        noTopThread = "Subthread: "+ subRedditName +". Does not have thread with score greather than 5000"
+        bot.sendMessage(chatId,noTopThread);
+      }
+      else{
+        subReddit[subRedditName].forEach(function(thread){
+          textMessage = "<b>"+thread["threadTitle"]+"</b> \n <em>SubReddit:"+ thread["subreddit"]+ " Votes: "+ thread["upvotes"]+"</em>\n "+thread["commentaryLink"]+" \n"
+          bot.sendMessage(chatId,textMessage,{parse_mode : "HTML"});
+        })
+      }
+    });
+}
+
+bot.onText(/\/NadaPraFazer (.+)/, (msg, match) => {
+  
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+  const subReddits = match[1].split(";");
+  
+  Reddit(subReddits,function(error,subReddits){
+      if(error){
+        sendFailedMessage(chatId,error);
+      }
+      else{
+        sendSuccesfulMessage(chatId,subReddits);
+      }
+  });
+});
+
+
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
